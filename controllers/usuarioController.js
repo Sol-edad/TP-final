f4af36e35c3b8dee98f2038a541fa14a84beef12
soledad <sol.rodriguez001@gmail.com>
2022-01-14 18:13:14 -0300

    logout

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -93,14 +93,16 @@ module.exports = {
     },
 
     logout: function (req,res) {
-        if (req.session.usuarioLogueado != undefined) {
-            res.redirect("/");
-        }
-        req.session.usuarioLogueado = undefined;
-        res.clearCookie('idUsuario');
-        return res.redirect('/');
-    },
+        req.session.destroy()
 
+        if (req.cookies.usuarioLogueado){
+            res.cookie('idUsuario', '',{maxAge: -1} )
+        }
+          
+           res.redirect("/");
+        }
+       
+,
     comentarios: function (req, res) {
         if (req.session.usuarioLogueado == undefined) {
             res.redirect("/");
